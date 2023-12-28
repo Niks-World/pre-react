fe63f7a41f3d6f7d5c4b0fe9292bb55bcdbc30e0
Niks-World <nikkalyan52@gmail.com>
2023-12-28 15:25:55 +0530

    sort work

--- product/src/componet/Test.jsx
+++ product/src/componet/Test.jsx
@@ -4,38 +4,52 @@ import { useEffect } from 'react'
 export const Test = () => {
 
   const[state, setState] = useState([])
+  const[sort ,setSort]=useState("");
+  const [filter, setFilter] = useState("")
 
-  console.log(state)
+  console.log(sort)
 
     useEffect(()=>{
       fatchData()
-    },[])
+    },[sort])
 
   const fatchData= async()=>{
+   let url= `http://localhost:8080/Product`
 
-    const resp= await fetch("http://localhost:8080/Product?_sort=price&_order=asc&_limit=5")
+   if(sort==="Low To High"){
+    url+="?_sort=price&_order=asc"
+   }else
+   if(sort==="High To Low"){
+    url+="?_sort=price&_order=desc"
+   }
+  //  if(sort==="none"){
+  //   url="http://localhost:8080/Product"
+  //  }
+    const resp= await fetch(url)
     const data= await resp.json()
     
     //console.log(data)
     setState(data)
   }
-
+const handleSort=(e)=>{
+setSort(e.target.value)
+}
 
   return (
     <div>
        <div>
         <h1>NiKhil Store</h1>
-        <select name="price" id=""> 
-        <option value="">select</option>
-          <option value="low">Low To High</option>
-          <option value="hig">High To Low</option>
+        <select onChange={handleSort}> 
+        <option >Sort by</option>
+          <option>Low To High</option>
+          <option >High To Low</option>
         </select>
-        <select name="cat" id="">
-          <option value="">category</option>
-          <option value="">Man's Cloths</option>
-          <option value="">Women's Cloths</option>
-          <option value="">Electonic</option>
-          <option value="">Jewllery</option>
+        <select >
+          <option >category</option>
+          <option >Man's Cloths</option>
+          <option >Women's Cloths</option>
+          <option >Electonic</option>
+          <option >Jewllery</option>
 
         </select>
 
